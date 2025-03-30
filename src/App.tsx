84aded45b7cd0a8e5a56aec4617652a24df7ab4e
Eduardo Ferro <eduardo.ferro@clarity.ai>
2025-03-30 23:19:57 +0200

    refactor: Simplify project structure and remove unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import logoHorizontal from './assets/logo-horizontal.png'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
@@ -21,7 +15,6 @@ function App() {
             </div>
             <div className="divide-y divide-gray-200">
               <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                <h1 className="text-3xl font-bold text-gray-900 mb-8">eferro's picks</h1>
                 <p className="text-gray-600">
                   A curated collection of talks about software development, XP, lean, agile, 
                   product management, cloud, operations, and technology.
